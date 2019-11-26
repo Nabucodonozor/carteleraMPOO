6ed7ddfb47c4e69b0c7750bdbcc8962152e02cd1
Nabucodonozor <42397072+Nabucodonozor@users.noreply.github.com>
2019-11-26 12:15:52 -0600

    Add files via upload

---
+++ Cartelera/FifthViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FifthViewController.swift
+//  Cartelera
+//
+//  Created by 2020-1 on 11/22/19.
+//  Copyright © 2019 Abstergo. All rights reserved.
+//
+
+import UIKit
+
+class FifthViewController: UIViewController {
+    
+    var totalFiV: [Ticket]!
+
+    @IBOutlet weak var final: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        var t: Int = 0
+        var s: Float = 0
+        for i in 0...totalFiV.count-1 {
+            t = t + totalFiV[i].ticket.count
+            s = s + totalFiV[i].subtotal
+        }
+        
+        final.text = "Transaccion exitosa,\n\tusted adquirió un total de \(t) boletos\n\n\tTOTAL: $\(s)"
+    }
+
+}
---
+++ Cartelera/Pelicula.swift
@@ -0,0 +1,22 @@
+//
+//  Pelicula.swift
+//  Cartelera
+//
+//  Created by 2020-1 on 10/3/19.
+//  Copyright © 2019 Abstergo. All rights reserved.
+//
+
+import UIKit
+
+struct Pelicula {
+    var poster: String
+    var titulo: String
+    var horarios: [String]
+    var costo: Float
+    
+    //9/10/2019
+    var sala: String
+    
+    //21/10/2019
+    var lugares: [Int]
+}
